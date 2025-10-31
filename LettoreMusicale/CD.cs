15c54042e174b7c93bab5045fe41a44c42c67173
Matteo Mancini <MATTEO.MANCINI@local.ittcesena.it>
2025-10-31 09:42:45 +0100

    Versione parziale clase cd

--- LettoreMusicale/CD.cs
+++ LettoreMusicale/CD.cs
@@ -8,5 +8,40 @@ namespace LettoreMusicale
 {
     class CD
     {
+        private string _Titolo;
+        private string _Autore;
+        private List<string> _ListaBrani;
+
+        public CD(string titolo, string autore, List<string> listaBrani)
+        {
+            _Titolo = titolo;
+            _Autore = autore;
+            _ListaBrani = listaBrani;
+        }
+
+        public string GetTitolo()
+        {
+            return _Titolo;
+        }
+
+        public string GetAutore()
+        {
+            return _Autore;
+        }
+
+        public void SetTitolo(string titolo) 
+        {
+            _Titolo = titolo;
+        }
+
+        public void SetAutore(string autore) 
+        {
+            _Autore = autore;
+        }
+
+        public override string ToString() 
+        {
+            
+        }
     }
 }
